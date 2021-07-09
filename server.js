a03d455a7dd29b982c0d67e41b1602c37b6a8fc7
BoDee <nyx0hemir@gmail.com>
2021-07-09 14:12:11 -0600

    finished first issue

--- server.js
+++ server.js
@@ -13,14 +13,17 @@ function filterByQuery(query, animalsArray) {
         } else {
             personalityTraitsArray = query.personalityTraits;
         }
-
+        // iterate through every item in the personalityTraits array, and perform sorting actions while storing the current item in "trait".
         personalityTraitsArray.forEach(trait => {
+            // iterate through every single object in the filteredResults array and perform a function that returns either true or false. If the return is truthy, keep object, if it is falsy, slice object, then set this new array to the old array's value.
             filteredResults = filteredResults.filter(
+                // return true as long as the index of the personality trait is any number besides -1, of which indexof will only return if said personality trait does not exist in the personality traits.
                 animal => animal.personalityTraits.indexOf(trait) !== -1
             );
         });
     }
     if (query.diet) {
+        // filtered results is set equal to the filtered array of filteredResults, which is filtered using a function that returns either true or false based upon if the current item is equal to the query's item, and if it is keep it.
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
     if (query.species) {
@@ -32,12 +35,23 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get("/api/animals", (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
     }
-    res.json(results);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+    const result = findById(req.params.id, animals);
+        res.json(result);
 });
 
 app.listen(PORT, () => {
