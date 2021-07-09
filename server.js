7aceabf0a07b70a6efb44401a3bcf206e020ed27
BoDee <nyx0hemir@gmail.com>
2021-07-08 19:50:55 -0600

    Add Heroku

--- server.js
+++ server.js
@@ -2,8 +2,41 @@ const express = require("express");
 const app = express();
 const { animals } = require("./data/animals");
 
-app.get("./api/animals.json", (req, res) => {
-    res.send("Hello!");
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === "string") {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+app.get("/api/animals", (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
 
 app.listen(3001, () => {
