678fa4cbd030cc1f493edcadae1d711b64b948fb
BoDee <nyx0hemir@gmail.com>
2021-07-10 13:11:51 -0600

    finished issue three

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 const { animals } = require("./data/animals");
 
 function filterByQuery(query, animalsArray) {
@@ -81,6 +82,22 @@ app.get("/api/animals", (req, res) => {
 app.get("/api/animals/:id", (req, res) => {
     const result = findById(req.params.id, animals);
         res.json(result);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.post("/api/animals", (req, res) => {
